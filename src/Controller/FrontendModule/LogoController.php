1b4d7e9bae0ad4e2651e3698f6f3179f1e43b4e1
Dennis Schmidt <de-es@users.noreply.github.com>
2025-07-22 21:15:43 +0200

    Update module controller

--- src/Controller/FrontendModule/LogoController.php
+++ src/Controller/FrontendModule/LogoController.php
@@ -14,21 +14,19 @@ namespace DeEs\ContaoLogo\Controller\FrontendModule;
 
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
-use Contao\CoreBundle\Image\Studio\Studio;
 use Contao\CoreBundle\Routing\ContentUrlGenerator;
 use Contao\CoreBundle\Twig\FragmentTemplate;
+use Contao\FilesModel;
 use Contao\ModuleModel;
 use Contao\PageModel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Exception\ExceptionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[AsFrontendModule(category: 'miscellaneous')]
 class LogoController extends AbstractFrontendModuleController
 {
     public function __construct(
-        private readonly Studio $studio,
         private readonly ContentUrlGenerator $urlGenerator,
     ) {
     }
@@ -39,31 +37,31 @@ class LogoController extends AbstractFrontendModuleController
             return new Response();
         }
 
-        $figureBuilder = $this->studio->createFigureBuilder();
+        if (!$file = FilesModel::findByUuid($model->singleSRC)) {
+            return new Response();
+        }
 
-        $figureBuilder
-            ->fromUuid($model->singleSRC ?: '')
-            ->setSize($model->imgSize)
-        ;
+        if ('svg' === $file->extension) {
+            $svg = file_get_contents($file->path);
+        }
+
+        $fileMeta = $file->getMetadata($request->getLocale());
+
+        if (null !== $fileMeta) {
+            $template->set('alt', $fileMeta->getAlt());
+        }
 
         $page = PageModel::findById($model->jumpTo);
 
         if (null !== $page) {
-            try {
-                $url = $this->urlGenerator->generate($page, [], UrlGeneratorInterface::ABSOLUTE_URL);
-            } catch (ExceptionInterface) {
-                return new Response();
-            }
-            $figureBuilder->setLinkHref($url);
+            $url = $this->urlGenerator->generate($page, [], UrlGeneratorInterface::ABSOLUTE_URL);
         }
 
-        if (!empty($model->titleText)) {
-            $figureBuilder->setLinkAttribute('title', $model->titleText);
-        }
+        $template->set('image', $file->path);
+        $template->set('svg', $svg ?? null);
 
-        $figure = $figureBuilder->buildIfResourceExists();
-
-        $template->set('image', $figure);
+        $template->set('href', $url ?? null);
+        $template->set('title', $model->titleText);
 
         return $template->getResponse();
     }
